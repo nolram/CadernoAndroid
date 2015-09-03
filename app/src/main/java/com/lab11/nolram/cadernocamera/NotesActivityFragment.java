39e4f42bcd6652a6123e21984c9289f645a16a35
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2015-09-03 11:00:01 -0300

    Implementação teste do gerador de PDF Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- app/src/main/java/com/lab11/nolram/cadernocamera/NotesActivityFragment.java
+++ app/src/main/java/com/lab11/nolram/cadernocamera/NotesActivityFragment.java
@@ -1,15 +1,31 @@
 package com.lab11.nolram.cadernocamera;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.AsyncTask;
 import android.os.Build;
+import android.os.CancellationSignal;
+import android.os.ParcelFileDescriptor;
+import android.print.PageRange;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
+import android.print.PrintManager;
+import android.print.pdf.PrintedPdfDocument;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -24,6 +40,14 @@ import com.lab11.nolram.database.controller.FolhaDataSource;
 import com.lab11.nolram.database.model.Caderno;
 import com.lab11.nolram.database.model.Folha;
 import com.melnykov.fab.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
 
 
 /**
@@ -44,6 +68,21 @@ public class NotesActivityFragment extends Fragment {
 
     private FolhaDataSource folhaDataSource;
 
+    private List<Folha> folhas;
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_generate_pdf) {
+            printDocument();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -90,6 +129,8 @@ public class NotesActivityFragment extends Fragment {
         btnAddFolha = (FloatingActionButton) view.findViewById(R.id.fab_imagem);
         toolbar = (Toolbar) view.findViewById(R.id.toolbar);
 
+        setHasOptionsMenu(true);
+
         linearLayoutManager = new LinearLayoutManager(view.getContext());
         mRecyclerView.setLayoutManager(linearLayoutManager);
 
@@ -102,7 +143,9 @@ public class NotesActivityFragment extends Fragment {
         folhaDataSource = new FolhaDataSource(view.getContext());
         folhaDataSource.open();
 
-        mAdapter = new AdapterCardsFolha(folhaDataSource.getAllFolhas(fk_caderno));
+        folhas = folhaDataSource.getAllFolhas(fk_caderno);
+
+        mAdapter = new AdapterCardsFolha(folhas);
         mRecyclerView.swapAdapter(mAdapter, true);
 
         mRecyclerView.addOnItemTouchListener(
@@ -111,7 +154,7 @@ public class NotesActivityFragment extends Fragment {
                     public void onItemClick(View view, int position) {
                         Intent intent = new Intent(view.getContext(), FolhaActivity.class);
                         Bundle bundle = new Bundle();
-                        Folha folha = folhaDataSource.getAllFolhas(fk_caderno).get(position);
+                        Folha folha = folhas.get(position);
                         bundle.putString(Database.FOLHA_LOCAL_IMAGEM, folha.getLocal_folha());
                         bundle.putString(Database.FOLHA_TITULO, folha.getTitulo());
                         bundle.putString(Database.FOLHA_DATA, folha.getData_adicionado());
@@ -139,4 +182,153 @@ public class NotesActivityFragment extends Fragment {
         });
         return view;
     }
+
+
+
+    public class MyPrintDocumentAdapter extends PrintDocumentAdapter
+    {
+        Context context;
+        private int pageHeight;
+        private int pageWidth;
+        public PdfDocument myPdfDocument;
+        public int totalpages = 4;
+
+        public MyPrintDocumentAdapter(Context context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        public void onLayout(PrintAttributes oldAttributes,
+                             PrintAttributes newAttributes,
+                             CancellationSignal cancellationSignal,
+                             LayoutResultCallback callback,
+                             Bundle metadata) {
+            myPdfDocument = new PrintedPdfDocument(context, newAttributes);
+
+            pageHeight =
+                    newAttributes.getMediaSize().getHeightMils()/1000 * 72;
+            pageWidth =
+                    newAttributes.getMediaSize().getWidthMils()/1000 * 72;
+
+            if (cancellationSignal.isCanceled() ) {
+                callback.onLayoutCancelled();
+                return;
+            }
+
+            if (totalpages > 0) {
+                PrintDocumentInfo.Builder builder = new PrintDocumentInfo
+                        .Builder("print_output.pdf")
+                        .setContentType(PrintDocumentInfo.CONTENT_TYPE_DOCUMENT)
+                        .setPageCount(totalpages);
+
+                PrintDocumentInfo info = builder.build();
+                callback.onLayoutFinished(info, true);
+            } else {
+                callback.onLayoutFailed("Page count is zero.");
+            }
+        }
+
+        private boolean pageInRange(PageRange[] pageRanges, int page)
+        {
+            for (int i = 0; i<pageRanges.length; i++)
+            {
+                if ((page >= pageRanges[i].getStart()) &&
+                        (page <= pageRanges[i].getEnd()))
+                    return true;
+            }
+            return false;
+        }
+
+
+        private void drawPage(PdfDocument.Page page,
+                              int pagenumber) {
+            Canvas canvas = page.getCanvas();
+
+            pagenumber++; // Make sure page numbers start at 1
+
+            int titleBaseLine = 72;
+            int leftMargin = 54;
+
+            Paint paint = new Paint();
+            paint.setColor(Color.BLACK);
+            paint.setTextSize(40);
+            canvas.drawText(
+                    "Test Print Document Page " + pagenumber,
+                    leftMargin,
+                    titleBaseLine,
+                    paint);
+
+            paint.setTextSize(14);
+            canvas.drawText("This is some test content to verify that custom document printing works", leftMargin, titleBaseLine + 35, paint);
+
+            if (pagenumber % 2 == 0)
+                paint.setColor(Color.RED);
+            else
+                paint.setColor(Color.GREEN);
+
+            PdfDocument.PageInfo pageInfo = page.getInfo();
+
+
+            canvas.drawCircle(pageInfo.getPageWidth()/2,
+                    pageInfo.getPageHeight()/2,
+                    150,
+                    paint);
+        }
+
+
+        @Override
+        public void onWrite(final PageRange[] pageRanges,
+                            final ParcelFileDescriptor destination,
+                            final CancellationSignal cancellationSignal,
+                            final WriteResultCallback callback) {
+
+            for (int i = 0; i < totalpages; i++) {
+                if (pageInRange(pageRanges, i))
+                {
+                    PdfDocument.PageInfo newPage = new PdfDocument.PageInfo.Builder(pageWidth,
+                            pageHeight, i).create();
+
+                    PdfDocument.Page page =
+                            myPdfDocument.startPage(newPage);
+
+                    if (cancellationSignal.isCanceled()) {
+                        callback.onWriteCancelled();
+                        myPdfDocument.close();
+                        myPdfDocument = null;
+                        return;
+                    }
+                    drawPage(page, i);
+                    myPdfDocument.finishPage(page);
+                }
+            }
+
+            try {
+                myPdfDocument.writeTo(new FileOutputStream(
+                        destination.getFileDescriptor()));
+            } catch (IOException e) {
+                callback.onWriteFailed(e.toString());
+                return;
+            } finally {
+                myPdfDocument.close();
+                myPdfDocument = null;
+            }
+
+            callback.onWriteFinished(pageRanges);
+        }
+
+    }
+
+    public void printDocument()
+    {
+        PrintManager printManager = (PrintManager) getActivity()
+                .getSystemService(Context.PRINT_SERVICE);
+
+        String jobName = this.getString(R.string.app_name) +
+                " Document";
+
+        printManager.print(jobName, new MyPrintDocumentAdapter(getActivity()),
+                null);
+    }
+
 }
--- app/src/main/java/com/lab11/nolram/cadernocamera/SettingsActivity.java
+++ app/src/main/java/com/lab11/nolram/cadernocamera/SettingsActivity.java
@@ -1,36 +1,24 @@
 package com.lab11.nolram.cadernocamera;
 
-import android.graphics.pdf.PdfDocument;
-import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 
-public class NotesActivity extends AppCompatActivity {
+public class SettingsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+        setContentView(R.layout.activity_settings);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_notes, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
@@ -39,9 +27,13 @@ public class NotesActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
-
 }
