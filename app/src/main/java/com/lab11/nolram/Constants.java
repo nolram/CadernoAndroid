d16d467b10f1c7a54d41f8ab0fb1ef441242789d
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2015-10-20 17:22:06 -0200

    - DEBUG = false; Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- app/src/main/java/com/lab11/nolram/Constants.java
+++ app/src/main/java/com/lab11/nolram/Constants.java
@@ -4,5 +4,5 @@ package com.lab11.nolram;
  * Created by nolram on 15/10/15.
  */
 public class Constants {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
